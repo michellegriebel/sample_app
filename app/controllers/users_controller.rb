0db975259559284961293ee8f174dac974de4a5a
michellegriebel <mgriebel@enventis.com>
2015-11-23 02:53:01 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
 
-  def new
+  def show
+    # to get the user show view to work, we defined an @user variable 
+    # params[:id] is the string "1" but find is smart enough to convert this to an integer.
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
